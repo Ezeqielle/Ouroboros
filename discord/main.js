7f92d5c7e372c8ff2550c9564df14bab26717624
Mr Wolf <mathis.dimascio@gmail.com>
2020-07-29 16:57:31 +0200

    start

--- discord/main.js
+++ discord/main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 global.discordJsClient = client
 
-const config = require('config.json')
+const config = require('./config.json')
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
