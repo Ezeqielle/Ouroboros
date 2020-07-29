15f4099644510b32dd90394b811ebcfd8da51492
Mr Wolf <mathis.dimascio@gmail.com>
2020-07-29 16:49:11 +0200

    update

---
+++ discord/main.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+global.discordJsClient = client
+
+const config = require('config.json')
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+console.log('[Discord]', 'Logging in...')
+client.login(config.bot_token)
